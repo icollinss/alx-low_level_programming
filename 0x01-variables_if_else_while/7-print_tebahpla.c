6e35eebdc516fec85d4084dce17f0504992f82da
root <icollinss@github.com>
2022-03-11 12:11:06 -0800

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description - prints lowercase alphabet in reverse
+ * using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('n');
+
+	return (0);
+}
