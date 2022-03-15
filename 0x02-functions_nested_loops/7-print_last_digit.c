89e35bebb3b403a4245aca5ccc479eff8a1633c2
root <icollinss@github.com>
2022-03-15 11:31:33 -0700

    loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main
+ * print last digit - returns last digit
+ * @a: input
+ * Return: 0 or 1
+ */
+int print_last_digit(int a);
+{
+	int b;
+
+	b = a % 10;
+
+	if (b < 0)
+	{
+		b = b * -1;
+	}
+	_putchar(b + '0');
+	return (b);
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
