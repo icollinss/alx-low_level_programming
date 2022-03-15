c9dddf2e83a1a0fbed3ba8d91316f692c7035a8d
root <icollinss@github.com>
2022-03-15 06:08:10 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * print '_putchar'
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char c[10] = "Putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+	_putchar(c[i]);
+	i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
