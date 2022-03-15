7362de522cbc63f2c549928fea818d01fe0b37f1
root <icollinss@github.com>
2022-03-15 09:06:13 -0700

    loops

---
+++ 0x02-functions_nested_loops/3_islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase characters
+ * @c : character to check the case:
+ * return: 1 on success or 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
